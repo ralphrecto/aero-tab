d770bd2e93d4f6ad938e30d17b9ff7310c457fac
Ralph Recto <recto.ralph@gmail.com>
2013-08-19 01:54:50 -0400

    Got background and content script talking to each other. Now designing UI

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,7 +1,31 @@
 (function(){
+  var globals = {
+    domain_regex: new RegExp("[^/]+://(www\.)?([^/]+)")
+  };
+
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    if(request.type === "get_urls"){
+      chrome.tabs.getAllInWindow(null, function(tabs){
+        var domains = {};
+        for(var i = 0; i < tabs.length; i++){
+          //group tabs by domain
+          var domain = globals.domain_regex.exec(tabs[i].url)[2];
+          if(domains[domain]){
+              domains[domain].push(tabs[i]);
+          } else {
+              domains[domain] = [tabs[i]];
+          }
+        }
+        sendResponse(domains);
+      });
+      //listener must return true for response to be sent
+      return true;
+    }
+  });
+
   chrome.commands.onCommand.addListener(function(command) {
-    alert("HELLO THIS IS TAB");
     chrome.tabs.insertCSS(null, {file: "stylesheets/page_dialog.css"});
     chrome.tabs.executeScript(null, {file: "javascripts/page_dialog.js"});
   });
+
 })();
--- javascripts/page_dialog.js
+++ javascripts/page_dialog.js
@@ -1,4 +1,30 @@
 (function(){
-  var container = document.createElement("div");
-  container.id = "aero-tab-dialog";
+  var globals = {
+    containerId: "aero-tab-dialog"
+  };
+
+  var container = document.getElementById(globals.containerId);
+  if(container){
+    document.body.removeChild(container);
+  } else {
+    chrome.runtime.sendMessage({type: "get_urls"}, function(response){
+      var domain_ul = document.createElement("ul");
+
+      //add domain groups to container
+      for(domain in response){
+        if(response.hasOwnProperty(domain)){
+          var favurl = response[domain][0].favIconUrl || "default_favicon.png";
+          var li = document.createElement("li");
+          var img = document.createElement("img");
+          img.setAttribute("src", favurl);
+          li.appendChild(img);
+          domain_ul.appendChild(li);
+        }
+      }
+      var container = document.createElement("div");
+      container.id = globals.containerId;
+      container.appendChild(domain_ul);
+      document.body.appendChild(container);
+    });
+  }
 })();
